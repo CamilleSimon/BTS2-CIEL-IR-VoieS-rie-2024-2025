9ec4819d6d8ef37cedcc99620b2c238c25c1ee5c
LAGNEAUGregory <165809499+LAGNEAUGregory@users.noreply.github.com>
2024-09-06 11:33:00 +0200

    Create TP Code César partie 1.c

---
+++ TP Code César partie 1.c
@@ -0,0 +1,24 @@
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    char readChar = 0; // caractère à lire
+
+    int buff = Serial.available();
+	int key = 6;
+  
+    while(buff > 0) // tant qu'il reste des caractères dans le buffer
+    {
+        readChar = Serial.read(); // lecture du caractère
+      	if (readChar + key >= 123)
+        {
+          readChar -= 26;
+        }
+      	char charcode = readChar + key;
+        Serial.print(charcode); // envoi du caractère
+        buff = Serial.available(); // mise à jour du nombre de caratères restant
+    }
+}
