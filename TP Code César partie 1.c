0647cf8511d6715df06c7d7e6d8341ee35f7685c
wassimyakoulti <wassim.yk33@gmail.com>
2024-09-10 17:38:08 +0200

    Create TP Code César partie 1.c

---
+++ TP Code César partie 1.c
@@ -0,0 +1,25 @@
+// C++ code
+//
+int key = 6;
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+    char readChar = 0; // caractère à lire
+
+    int buff = Serial.available();
+
+    while(buff > 0) // tant qu'il reste des caractères dans le buffer
+    {
+        readChar = Serial.read(); // lecture du caractère
+     if (readChar>=117) {
+      readChar = readChar-26;
+      }
+      readChar = readChar + key;
+        Serial.print(readChar); // envoi du caractère
+        buff = Serial.available(); // mise à jour du nombre de caratères restant
+    }
+}
