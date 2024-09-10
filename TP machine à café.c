83771d554d94205bc082b352dad1a35b82fb0f95
MotLev <mat.motlev6909@gmail.com>
2024-09-10 12:05:47 +0200

    Create TP machine à café.c

---
+++ TP machine à café.c
@@ -0,0 +1,51 @@
+const int COUNT = 3;
+const int PINLEDS[COUNT] = {13,12,11};
+const int PINBUTTON[COUNT] = {4,3,2};
+const int STOCKSBOISSON[COUNT] = {0,0,0};
+const String BOISSON[COUNT] = {"cafe", "chocolat", "the"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void remplir( int type_boisson) {
+   
+        STOCKSBOISSON[type_boisson] = 10;//mettre le stock à dix
+}
+
+void receptioncommande()
+{
+  int buff = 0
+  buff = receptioncommande();
+  
+  while(buff > 0)
+  {
+   STOCKBOISSON  
+    //Serial.begin(9600);
+    pinMode(13, OUTPUT);
+  }
+  
+  
+}
+  
+void setup()
+
+ 
+
+
+void loop()
