4be2806f2253a08852e4c34a76a9fae7775ed9a4
Osama Taliby <talibyosama@gmail.com>
2024-09-11 08:11:19 +0200

    Create TP Code César partie 1.c

---
+++ TP Code César partie 1.c
@@ -0,0 +1,27 @@
+int key = 6;
+
+void setup()
+{
+   Serial.begin(9600);
+
+}
+
+void loop()
+{
+  int buff = Serial.available();
+  char readChar;
+  
+  while(buff > 0){
+  readChar = Serial.read();
+  int intChar = readChar + key;
+  if(intChar > 122)
+  {
+    intChar -= 26;
+  }
+  
+  readChar = intChar;
+  Serial.print(readChar);
+  
+  buff = Serial.available();
+	}
+}
