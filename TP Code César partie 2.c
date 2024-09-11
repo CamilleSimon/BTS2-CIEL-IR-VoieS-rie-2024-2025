2bf0fb6275be359678ae566d531c05534f90a7cc
Osama Taliby <talibyosama@gmail.com>
2024-09-11 17:46:14 +0200

    Create TP Code César partie 2.c

---
+++ TP Code César partie 2.c
@@ -0,0 +1,53 @@
+int key;
+int operation; /// 0 : le mssage doit etre chiffre , 1 : le message doit etre dechiffre
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("envoyer un message sous la forme : operation,cle,mot");
+}
+
+void loop(){
+
+  int buff = Serial.available();
+  char readChar;
+  int tab[256];
+  int i=0;
+  while(buff > 0) {
+  readChar = Serial.read();
+  tab[i]= readChar;
+  i++;
+  }
+
+
+}
+
+String chiffrement(key)
+{
+  int intChar = readChar + key;
+  if(intChar > 122)
+  {
+    intChar -= 26;
+  }
+  
+  readChar = intChar;
+  Serial.print(readChar);
+  
+  buff = Serial.available();
+    }
+}
+
+String dechiffrement(key)
+{
+  int intChar = readChar - key;
+  if(intChar > 122)
+  {
+    intChar -= 26;
+  }
+  
+  readChar = intChar;
+  Serial.print(readChar);
+  
+  buff = Serial.available();
+    }
+}
