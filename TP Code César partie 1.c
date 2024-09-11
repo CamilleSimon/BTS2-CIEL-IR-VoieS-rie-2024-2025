202f93c622531993b1524de8a891555f2b34884b
Marcon Manoah <manoah.marcon@gmail.com>
2024-09-11 16:23:34 +0200

    Update TP Code César partie 1.c

--- TP Code César partie 1.c
+++ TP Code César partie 1.c
@@ -14,14 +14,18 @@ void loop()
     while(buff > 0) // tant qu'il reste des caractères dans le buffer
     {	
       	char buff2 = Serial.read(); // caractère à lire
-      	int buff3 = buff2 + key;
-      
+		
+		int buff3 = buff2 + key; 
+
       if (buff3 > 122){
-        	buff3 = buff3 - 26;
-        	buff2 = buff3;
+		buff3 = buff3 - 26;
+		buff2 = buff3;
+      }
+      else 
+      {
+        buff2 = buff2 + key;
       }
         Serial.print(buff2); // envoi du caractère
         buff = Serial.available(); // mise à jour du nombre de caratères restant
     }
 }
-      	
