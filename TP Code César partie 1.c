f99ab6e7eb62428c8777b8b76731bb0af3dd71eb
BrianSGUON <165809873+BrianSGUON@users.noreply.github.com>
2024-09-13 11:50:13 +0200

    Create TP Code César partie 1.c

---
+++ TP Code César partie 1.c
@@ -0,0 +1,25 @@
+const int key = 6; //declaration de la constante key 
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    char readChar = 0; // caractère à lire
+
+    int buff = Serial.available();
+
+    while(buff > 0) // tant qu'il reste des caractères dans le buffer
+    {
+        readChar = Serial.read(); // lecture du caractère
+      	readChar = readChar + key; // ajout de la cle au caractere
+      	if (readChar > 'z')// si apres l'ajout de la cle le caractere depasse z on revient à a
+        {
+            readChar = readChar - 26; // Revenir à 'a' après 'z'
+        }
+        Serial.print(readChar); // envoi du caractère
+        buff = Serial.available(); // mise à jour du nombre de caratères restant
+    }
+}
