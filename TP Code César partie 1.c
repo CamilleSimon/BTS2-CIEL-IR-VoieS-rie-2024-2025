ce0f67070c5c8200ec61e8444f4734d8525a13f3
PlonoXxcoder <92154741+PlonoXxcoder@users.noreply.github.com>
2024-09-13 08:16:09 +0200

    Update TP Code César partie 1.c

--- TP Code César partie 1.c
+++ TP Code César partie 1.c
@@ -32,3 +32,39 @@ void loop()
     buff = Serial.available();
   }
 }
+```c++
+int key = 6;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char readChar = 0; // caractère à lire
+
+  int buff = Serial.available();
+
+  int charcesar;
+
+  while (buff > 0)
+  {
+    readChar = Serial.read(); // lecture du caractère
+
+    charcesar = key + readChar;
+
+    // Gestion du dépassement pour les lettres en fin d'alphabet
+    if (charcesar > 'z') {
+      charcesar = 'a' + (charcesar - 'z' - 1);
+      
+    }
+    
+	char result = charcesar;
+   // Serial.print(charcesar); // envoi du caractère
+    int test = charcesar;
+    Serial.print(test);
+    buff = Serial.available();
+  }
+}
+```
