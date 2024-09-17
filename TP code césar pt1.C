b6390a46bdbf4e0fcdb04e8a60491b11ed7c7ca1
JrsY <yague.jauris@gmail.com>
2024-09-17 14:51:35 +0200

    TP code césar pt1.C

---
+++ TP code césar pt1.C
@@ -0,0 +1,31 @@
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{  
+	char readChar = 0; // caractère à lire
+	int key = 6;
+	int buff = Serial.available(); // mise à jour du nombre de caratères restant
+
+    if(buff > 0)
+    {
+        	while(buff > 0) // tant qu'il reste des caractères dans le buffer
+    		{
+       		readChar = Serial.read(); // lecture du caractère
+      
+      		readChar = readChar + key; // Pour chiffrer un message
+      
+      
+      		if(readChar > 122)
+       	 	{
+         	readChar = readChar - 26 ; 
+        	}
+      
+      
+        	Serial.print(readChar); // envoi du caractère
+        	buff = Serial.available(); // mise à jour du nombre de caratères restant 		
+            }
+     }
+}      
