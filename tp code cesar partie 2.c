3d32e453fdb9c3f8a0ac8f473f627d391527a02f
rayane <raayanemerdji@gmail.com>
2024-09-18 17:51:45 +0200

    Update tp code cesar partie 2.c

--- tp code cesar partie 2.c
+++ tp code cesar partie 2.c
@@ -1,75 +1,82 @@
-int key; // definir une variable
-int operation; // definir 
-int message;
+int key, operation, message; // definir une variable
 
 void setup()// crée une fonction
 {
-    Serial.begin(9600);
+  Serial.begin(9600);
+
+
 }
 
 void loop()// crée une fonction
 { 
-    
-   char readChar = 0; // caractère à lire
-   int buff = Serial.available();
-
-    while(buff > 0) // tant qu'il reste des caractères dans le buffer
-    {
-        readChar = Serial.read(); // lecture du caractère
-        buff = Serial.available(); // mise à jour du nombre de caratères restant
-        tab[i] = readChar; //tab[i]=acceder à une case du tableau readchar=lire la
-        i++;
-    }
-      
-    if (operation = 0)
-    {
-      chiffrement;
-     
-    }
-    else 
-    {
-       dechiffrement;
-    }
-      
-    int tableau[256];
-  
-  tableau[0]= operation;
-  tableau[1]= key;
-  tableau[2]= message;
-}
-void chiffrement()// crée une fonction
-{
-    
-  
+  int buff = Serial.available();
   char readChar = 0; // caractère à lire
-
-    int buff = Serial.available();
-
-    while(buff > 0) // tant qu'il reste des caractères dans le buffer
-    {
-        readChar = Serial.read(); // lecture du caractère
-        readChar = readChar +key; // caractère à lire
-        Serial.print(readChar); // envoi du caractère
-        buff = Serial.available(); // mise à jour du nombre de caratères restant
-      if (readChar>122);
-      {
-       readChar = readChar -26; 
+  String s="";
+ 
+  while(buff > 0)
+  {
+    readChar=Serial.read();  // lecture du caractère
+    s= s + readChar; //rajouter a s le contenu de readChar
+    buff=Serial.available(); // mise à jour du nombre de caratères restant
+  }
+  if (s != "")
+  {
+  	String stringOne = "0,6,bonjour";
+  // substring(index) looks for the substring from the index position to the end:
+  Serial.print(stringOne.substring(0,1));
+  operation = stringOne.substring(0,1).toInt();
+  Serial.print(operation);
   
+ 
 
+  Serial.print(stringOne.substring(2,4));
+  stringOne = "<HTML><HEAD><BODY>";
+int lastOpeningBracket = stringOne.lastIndexOf('<');
+
+
+  // substring(index) looks for the substring from the index position to the end:
+  Serial.print(stringOne.substring(4,10));
+  Serial.print (s);
+  }
+  if (operation ==48 )
+  {
+    chiffrement(s);   
+  }
+  
+  else 
+  {
+   if (operation==49)
+   {
+    dechiffrement(s);
+   }
+  }
 }
-void dechiffrement()// crée une fonction
+
+
+
+
+ void chiffrement(String message)// crée une fonction
 {
-   char readChar = 0; // caractère à lire
-
-    int buff = Serial.available();
-
-    while(buff > 0) // tant qu'il reste des caractères dans le buffer
-    {
-        readChar = Serial.read(); // lecture du caractère
-        readChar = readChar -key; // caractère à lire
-        Serial.print(readChar); // envoi du caractère
-        buff = Serial.available(); // mise à jour du nombre de caratères restant
-      if (readchar>122);
-      {
-       readchar = readchar -26; 
+  for (int i=0; i<message.length(); i++)
+  {
+    message [i]+=key;
+    if(message.charAt(i)>'z')
+    message[i]-=26;
+  }
+    Serial.print(message);
 }
+
+
+
+
+ void dechiffrement(String message)// crée une fonction
+{
+  for (int i=0; i<message.length(); i++)
+  {
+    message [i]-=key;
+    if(message.charAt(i)>'z')
+    message[i]+=26;
+  }
+    Serial.print(message);
+}
+
