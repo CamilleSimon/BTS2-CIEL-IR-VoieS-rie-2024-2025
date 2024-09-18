8110c784126d12ea3bde33631bedb5d336c8636d
1001-aassil <153134860+1001-aassil@users.noreply.github.com>
2024-09-18 14:28:51 +0200

    Update tp cesar part1.c

--- tp cesar part1.c
+++ tp cesar part1.c
@@ -1,33 +1,36 @@
 void setup()
 {
-    Serial.begin(9600)
+    Serial.begin(9600);
 }
 
 void loop()
 {
 	char readChar = 0 ; // caractère à lire
   	char key = 6; // valeurs de la key
-  
-  
+
+
     int buff = Serial.available();
 
     while(buff > 0 ) // tant qu'il reste des caractères dans le buffer
     {
         readChar = Serial.read(); // lecture du caractère
-        char cChiff = readChar + key ; 
+        int cChiff = readChar + key ; 
       
+      
+
       if( cChiff > 122)
       {
-        char cReste = cChiff - 122;
-        char cChiff1 = 97 + cReste;
+        char cChiff1 = cChiff - 26;
+        
         Serial.print(cChiff1); // envoi du caractère
         buff = Serial.available(); // mise à jour du nombre de caratères restant
-        break ; 
       }
+      
       else 
       {
-        Serial.print(cChiff); // envoi du caractère
+        Serial.write(cChiff); // envoi du caractère
         buff = Serial.available(); // mise à jour du nombre de caratères restant
       }
     }
 }
+     
