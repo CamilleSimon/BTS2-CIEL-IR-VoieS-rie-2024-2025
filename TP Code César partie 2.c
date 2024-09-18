1d0088b0315473f22569d5bf92b6f5951fa71c4c
1001-aassil <153134860+1001-aassil@users.noreply.github.com>
2024-09-18 17:53:47 +0200

    Update TP Code César partie 2.c

--- TP Code César partie 2.c
+++ TP Code César partie 2.c
@@ -1,98 +1,88 @@
-je n'est pas toutes assemblée je vous met les differentes parties 
 
-  DECHIFFREMENT 
+
 
 void setup()
 {
     Serial.begin(9600);
-   
-  	
+
+  
 }
 
 void loop()
 {
-	char readChar = 0 ; // caractère à lire
-  	char key = 12; // valeurs de la key
+  String mpr = "0,6,gdrgdrgd" ; // La taille du tableau chaine est automatiquement calculée
   
+  int ope = mpr.substring(0,1).toInt();
+  int key = mpr.indexOf(',',',');
   
+  int rt = mpr.indexOf(',', 2);
+  String mdp = mpr.substring(rt);
+  
+  if(ope = 0)
+  {
+
+
+
     int buff = Serial.available();
 
     while(buff > 0 ) // tant qu'il reste des caractères dans le buffer
     {
-        readChar = Serial.read(); // lecture du caractère
-        int cChiff = readChar - key ; 
-      
+        char cChiff = mdp - key ; 
+
       if( cChiff < 97)
       {
         char cChiff1 = cChiff + 26;
         Serial.print(cChiff1); // envoi du caractère
         buff = Serial.available(); // mise à jour du nombre de caratères restant
+        
+      }
+      else 
+      {
+        Serial.print(cChiff); // envoi du caractère
+        buff = Serial.available(); // mise à jour du nombre de caratères restant
+      }
+    }
+
+   
+  
+  else
+  {
+  
+    int buff1 = Serial.available();
+
+    while(buff1 > 0 ) // tant qu'il reste des caractères dans le buffer
+    {
+       
+        char cChiff1 = mdp + key ; 
+
+      if( cChiff1 > 122)
+      {
+        char cReste1 = cChiff1 - 122;
+        char cChiff11 = 97 + cReste1;
+        Serial.print(cChiff11); // envoi du caractère
+        buff1 = Serial.available(); // mise à jour du nombre de caratères restant
         break ; 
       }
       else 
       {
-        Serial.write(cChiff); // envoi du caractère
-        buff = Serial.available(); // mise à jour du nombre de caratères restant
+        Serial.print(cChiff); // envoi du caractère
+        buff1 = Serial.available(); // mise à jour du nombre de caratères restant
       }
     }
-}
-
-CHIFFREMENT 
-
-void setup()
-{
-    Serial.begin(9600);
-}
-
-void loop()
-{
-	char readChar = 0 ; // caractère à lire
-  	char key = 6; // valeurs de la key
-
-
-    int buff = Serial.available();
-
-    while(buff > 0 ) // tant qu'il reste des caractères dans le buffer
-    {
-        readChar = Serial.read(); // lecture du caractère
-        int cChiff = readChar + key ; 
-
-
-
-      if( cChiff > 122)
-      {
-        char cChiff1 = cChiff - 26;
-
-        Serial.print(cChiff1); // envoi du caractère
-        buff = Serial.available(); // mise à jour du nombre de caratères restant
-      }
-
-      else 
-      {
-        Serial.write(cChiff); // envoi du caractère
-        buff = Serial.available(); // mise à jour du nombre de caratères restant
-      }
-    }
-}
-
-
-OPPERATION 
-
-
-void setup()
-{
-    Serial.begin(9600);
-}
-
-void loop()
-{
-  char readChar = 0 ; // caractère à lire
-  int tab[10];
+  }
   
   
-  int buff = Serial.available();
   
-  while(buff > 0 ) // tant qu'il reste des caractères dans le buffer
-    {
+  
+  
+  
+  
+  
+ 
+  
+
   
 }
+
+
+
