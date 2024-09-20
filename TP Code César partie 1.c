58d9e8ba73105ae285b9f86262c355874a4f831a
Rose-Marie6tech <fayomirose@gmail.com>
2024-09-20 10:20:29 +0200

    Update TP Code César partie 1.c

--- TP Code César partie 1.c
+++ TP Code César partie 1.c
@@ -1,30 +1,30 @@
 // C++ code
 //
 int key = 6;
-
 void setup()
 {
+    // ouverture de la liaison à une vitesse de 9600 bits par seconde.
     Serial.begin(9600);
 }
-
 void loop()
 {
-    int readChar = 0; // caractère à lire
-
+  String text ="";
+  
+  	char readChar = 0; // caractère à lire
     int buff = Serial.available();
-
     while(buff > 0) // tant qu'il reste des caractères dans le buffer
     {
-        readChar = Serial.read()+ key; // lecture du caractère
-       char result;
-      if (readChar > 'z')
-      {
-        char result = readChar - 26;
-        Serial.print(readChar); // envoi du caractère
+        readChar = Serial.read(); // lecture du caractère
+        text = text+readChar;// ajouter le contenu de readChar dans text
         buff = Serial.available(); // mise à jour du nombre de caratères restant
-      }
-          
-   
-       
+      	//Serial.print(readChar);
     }
+    
+    for(int i = 0;i<=text.length();i++)
+    {
+      if (text[i]>='u')
+        text[i] =text[i]-26;
+      text[i] = text[i] + key;
+    }
+    Serial.print(text);
 }
