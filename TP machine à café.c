d9d4eefdd944aa942d165de4b12934394c9cf7e7
PlonoXxcoder <92154741+PlonoXxcoder@users.noreply.github.com>
2024-09-20 11:18:57 +0200

    Update TP machine à café.c

--- TP machine à café.c
+++ TP machine à café.c
@@ -19,9 +19,6 @@ void setup() {
 }
 
 void loop() {
-	Serial.println("Bouton");
-  	delay(2000);
-
     for (int i = 0; i < 3; i++) {
      	checkLumiere(i);  // Vérifie la lumière avant de servir une boisson
         if (digitalRead(boutons[i]) == LOW) {  // Bouton appuyé
