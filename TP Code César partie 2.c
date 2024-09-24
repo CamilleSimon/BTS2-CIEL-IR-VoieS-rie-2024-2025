6b89b85f4c601941eac736447776ede3ed1bdeda
Walid01100 <djelloul.walid01@gmail.com>
2024-09-24 12:20:10 +0200

    Update TP Code César partie 2.c

--- TP Code César partie 2.c
+++ TP Code César partie 2.c
@@ -1,35 +1,55 @@
-int key = 6;
+int key; 
+int jesaispas;
+String message = "";
+String jesaispas2 = "";
 void setup()
 {
     Serial.begin(9600);
 }
 
+    
+
+
+
+//code de la prof 
+void chiffrement(String message)
+{
+  for (int i = 0; i<message.length(); i++)//i parcourt tout le message 
+  {
+    message[i] = message[i] + key;
+  }
+  Serial.println(message);
+}
+
+
+
+void dechiffrement (String message)
+{
+  for (int i = 0; i<message.length(); i++)//i parcourt tout le message 
+  {
+    message[i] = message[i] - key;
+  }
+  Serial.println(message);
+}
+  
 void loop()
 {
     char readChar = 0; // caractère à lire
-   
+
     int buff = Serial.available();
 
     while(buff > 0) // tant qu'il reste des caractères dans le buffer
     {
         readChar = Serial.read(); // lecture du caractère
-      if (readChar >= 117)
-      {
-        readChar = readChar - 26;
-      }
-        readChar = readChar + key;
-        Serial.print(readChar);// envoi du caractère
+      jesaispas2 = readChar;
         buff = Serial.available(); // mise à jour du nombre de caratères restant
     }
-}
-
-//code de la prof 
-void chiffrement(String message)
-{
-  for (int i = 0; i<message.length(); i++)
+  jesaispas = jesaispas2[0];
+  key = s.substring(2, s.indexOf(',', 2));//il s'arrete a la deuxieme virgule non compris
+  message = s.substring(s.indexOf(',', 2)+1,);
+  if 
   {
-    message[i] = message[i] + key;
-  }
-  serial println(message);
+    jesaispas = 0
+    
 }
-
+  
